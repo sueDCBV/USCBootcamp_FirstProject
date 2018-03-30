3f3ed9acc482112ab03ad002829468a677c28254
lindsayyan <lindsayyan0311@gmail.com>
2018-03-29 19:13:58 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 gkey = "AIzaSyDhHFMqoQy1heeLIZjzaKHaMqSVtVVv2NY"
 gkey_places = "AIzaSyAQEoq29vRMq8kBxQxgBj7-gKiDX-ttcds"
+gkey1="AIzaSyBC0KeGBlUK4nn6b4nGdMUq-Dfuqq9ICbU"
+gkey2="VFkGlNc1HiP8D2iIgHoDzW5yAVdn3mXtl3r0wzKsFI53mBiaXSMYqt553zaS0Pcj"
