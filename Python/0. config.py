125f9eaf1967d4d5fdc0fe6d5444f96c79defac0
SUE HELLEN DEL CARPIO BELLIDO VARGAS <sueDCBV@gmail.com>
2018-04-02 19:29:59 -0700

    Add api key for census

--- Python/0. config.py
+++ Python/0. config.py
@@ -3,3 +3,4 @@ gkey_places = "AIzaSyAQEoq29vRMq8kBxQxgBj7-gKiDX-ttcds"
 gkey1="AIzaSyBC0KeGBlUK4nn6b4nGdMUq-Dfuqq9ICbU"
 gkey2="VFkGlNc1HiP8D2iIgHoDzW5yAVdn3mXtl3r0wzKsFI53mBiaXSMYqt553zaS0Pcj"
 zws_id = 'X1-ZWz1gba2odrvgr_6plsv'
+api_key = "ad9d7bc714dd32d3202f1718069b354ef0a2735b"
