698c7e99e117a9f74fbd12cd56c8ffeb26e606f3
ZL14E161110 <34262977+ZL14E161110@users.noreply.github.com>
2018-04-03 00:54:46 -0700

    UPdate file name and file config

--- Python/config.py
+++ Python/config.py
@@ -4,3 +4,4 @@ gkey1="AIzaSyBC0KeGBlUK4nn6b4nGdMUq-Dfuqq9ICbU"
 gkey2="VFkGlNc1HiP8D2iIgHoDzW5yAVdn3mXtl3r0wzKsFI53mBiaXSMYqt553zaS0Pcj"
 zws_id = 'X1-ZWz1gbvc8dh5vv_1vfab'
 api_key = "ad9d7bc714dd32d3202f1718069b354ef0a2735b"
+zipkey = "aBJPqUfiNCxoGUMdpUGdedHbovIzp8hrZsTOollpAvlaWnaq3nizIC91KMmThisq"
