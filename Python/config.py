0bcd424c4d1711009161672248824e3eea848202
ZL14E161110 <34262977+ZL14E161110@users.noreply.github.com>
2018-04-02 22:34:17 -0700

    update combine file

--- Python/config.py
+++ Python/config.py
@@ -2,5 +2,5 @@ gkey = "AIzaSyDhHFMqoQy1heeLIZjzaKHaMqSVtVVv2NY"
 gkey_places = "AIzaSyAQEoq29vRMq8kBxQxgBj7-gKiDX-ttcds"
 gkey1="AIzaSyBC0KeGBlUK4nn6b4nGdMUq-Dfuqq9ICbU"
 gkey2="VFkGlNc1HiP8D2iIgHoDzW5yAVdn3mXtl3r0wzKsFI53mBiaXSMYqt553zaS0Pcj"
-zws_id = 'X1-ZWz1gba2odrvgr_6plsv'
+zws_id = 'X1-ZWz1gbvc8dh5vv_1vfab'
 api_key = "ad9d7bc714dd32d3202f1718069b354ef0a2735b"
