dadaa960d3e0d7a945ee398c9c467cd5444fb1d8
SUE HELLEN DEL CARPIO BELLIDO VARGAS <sueDCBV@gmail.com>
2019-10-06 20:39:43 -0700

    p

--- Python/config.py
+++ Python/config.py
@@ -4,4 +4,4 @@ gkey1="AIzaSyDtfjSNS2DCSLMnclYaIL4oIFH-i2JmRc4"
 gkey2="VFkGlNc1HiP8D2iIgHoDzW5yAVdn3mXtl3r0wzKsFI53mBiaXSMYqt553zaS0Pcj"
 zws_id = 'X1-ZWz1gbvc8dh5vv_1vfab'
 api_key = "ad9d7bc714dd32d3202f1718069b354ef0a2735b"
-zipkey = "aBJPqUfiNCxoGUMdpUGdedHbovIzp8hrZsTOollpAvlaWnaq3nizIC91KMmThisq"
+zipkey = "6MPyNsDWkYW5xgQcS7DIAvPbUWsNX6lAcGU1MB86wNAJtu5KqYaa6COV4GeMecpK"
